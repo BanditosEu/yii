ad95cb119d954e73e7d6f944d876d43c1b38dab2
BanditosEu <91117366+BanditosEu@users.noreply.github.com>
2024-01-25 13:25:31 +0100

    create repo

---
+++ config/params.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    'languages' => [
+    //'da' => 'Danske',
+    //'de' => 'Deutsche',
+    //'en' => 'English', // NOT REQUIRED the sourceLanguage (i.e. the default)
+    'en-GB' => 'British English',
+    'en-US' => 'American English',
+    'es' => 'Español',
+    'fr' => 'Français',
+    'it' => 'Italiano',
+    //'ja' => '日本の',  // Japanese with the word "Japanese" in Kanji
+    //'nl' => 'Nederlandse',
+    //'no' => 'Norsk',
+    //'pl' => 'Polski',
+    'pt' => 'Português',
+    //'ru' => 'Русский',
+    //'sw' => 'Svensk',
+    //'zh' => '中国的',
+],
+
+];
